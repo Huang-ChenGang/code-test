bef2d717d9400ff4748af82655a358dcd5a9281e
Huang ChenGang <hcggang@163.com>
2022-01-05 10:37:46 +0800

    application start success

---
+++ src/main/java/com/cognizant/code/test/CodeTestApplication.java
@@ -0,0 +1,11 @@
+package com.cognizant.code.test;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CodeTestApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(CodeTestApplication.class, args);
+    }
+}
